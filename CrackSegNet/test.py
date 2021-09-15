06e4c6a603eadcba60ddba33e18090a4b57240de
preethamam <preethamam@gmail.com>
2021-09-15 00:35:51 -0700

    First commit.

---
+++ CrackSegNet/test.py
@@ -0,0 +1,42 @@
+import os
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID";
+os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+from model import *
+import skimage.transform as trans
+import tensorflow as tf
+import skimage.io as io
+import pickle
+import torch
+
+#model_name = './trained/4/ImprovedFCN_40'
+model_name = './ImprovedFCN'
+#model_name = './ImprovedFCN_origin'
+model = unet3s2()
+model.load_weights(model_name + '.hdf5')
+#CrackSegNet
+#test_path="../test_data/CrackSegNet/TestingCracks"
+#test_path = "../test_data/DS3/TestingCracks"
+test_path = "./Testing/Xincong/TestingCracks"
+data_name = os.listdir(test_path)
+
+softmax_dict = {}
+for i in data_name:
+   if 'predict' not in i:
+        img = io.imread(os.path.join(test_path, i)) 
+        size_x = img.shape[0]
+        size_y = img.shape[1]
+        img = trans.resize(img, (512, 512))
+        img = np.reshape(img, (1,) + img.shape)
+        results = model.predict(img, verbose=1)
+        # results = np.squeeze(results)
+        # results[results <= 0.5] = 0
+        # results[results > 0.5] = 1
+        prediction = torch.reshape(torch.from_numpy(results), (1, 1, 512, 512))  # convert to pytorch tensor
+        m = torch.nn.Upsample(size=(size_x,size_y), mode="nearest")  # upsampling
+        resized_prediction = m(prediction).numpy()  # upsampling
+        softmax_dict[i] = list(np.ndarray.flatten(resized_prediction))  # save flattened to dictionary
+        filename = i.replace('.jpg','.png')
+        # io.imsave(os.path.join("./ExperimentResults/pred", filename+".png"), results)
+        
+with open('soft_max_output.pickle', 'wb') as handle:
+    pickle.dump(softmax_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
---
+++ HRNet-Semantic-Segmentation/lib/datasets/DLCrack.py
@@ -0,0 +1,127 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) Microsoft
+# Licensed under the MIT License.
+# Written by Ke Sun (sunk@mail.ustc.edu.cn)
+# Referring to the implementation in 
+# https://github.com/zhanghang1989/PyTorch-Encoding
+# ------------------------------------------------------------------------------
+
+import os
+
+import cv2
+import numpy as np
+
+import torch
+from torch.nn import functional as F
+from PIL import Image
+
+from .base_dataset import BaseDataset
+
+class DLCrack(BaseDataset):
+    def __init__(self,
+                 root,
+                 list_path,
+                 num_samples=None,
+                 num_classes=2,
+                 multi_scale=True,
+                 flip=True,
+                 ignore_label=-1,
+                 base_size=520,
+                 crop_size=(480, 480),
+                 downsample_rate=1,
+                 scale_factor=16,
+                 mean=[0.485, 0.456, 0.406],
+                 std=[0.229, 0.224, 0.225]):
+
+        super(DLCrack, self).__init__(ignore_label, base_size,
+                crop_size, downsample_rate, scale_factor, mean, std)
+        self.root = root
+        self.num_classes = num_classes
+        self.list_path = list_path
+        self.class_weights = None
+
+        self.multi_scale = multi_scale
+        self.flip = flip
+        self.crop_size = crop_size
+
+        self.img_list = [line.strip().split() for line in open(root+list_path)]
+        self.files = self.read_files()
+        if num_samples:
+            self.files = self.files[:num_samples]
+
+    def read_files(self):
+        files = []
+        for item in self.img_list:
+            image_path, label_path = item
+            name = os.path.splitext(os.path.basename(label_path))[0]
+            sample = {
+                'img': image_path,
+                'label': label_path,
+                'name': name
+            }
+            files.append(sample)
+        return files
+
+    def resize_image(self, image, label, size):
+        image = cv2.resize(image, size, interpolation=cv2.INTER_LINEAR)
+        label = cv2.resize(label, size, interpolation=cv2.INTER_LINEAR)
+        return image, label
+
+    def __getitem__(self, index):
+        item = self.files[index]
+        name = item["name"]
+        image_path = os.path.join(self.root, item['img'])
+        label_path = os.path.join(self.root, item['label'])
+        image = cv2.imread(
+            image_path,
+            cv2.IMREAD_COLOR
+        )
+        label = np.array(
+            Image.open(label_path).convert('L')
+        )
+
+        image = cv2.resize(image, szie,interpolation=cv2.INTER_LINEAR)
+        label = cv2.resize(label, size, interpolation=cv2.INTER_LINEAR)
+    
+        label = label / 255
+        size = label.shape
+        if 'testval' in self.list_path:
+            image, border_padding = self.resize_short_length(
+                image,
+                short_length=self.base_size,
+                fit_stride=8,
+                return_padding=True
+            )
+            image = self.input_transform(image)
+            image = image.transpose((2, 0, 1))
+
+            return image.copy(), label.copy(), np.array(size), name, border_padding
+
+        if 'val' in self.list_path:
+            image, label = self.resize_short_length(
+                image,
+                label=label,
+                short_length=self.base_size,
+                fit_stride=8
+            )
+            image, label = self.rand_crop(image, label)
+            image = self.input_transform(image)
+            image = image.transpose((2, 0, 1))
+
+            return image.copy(), label, np.array(size), name
+        
+
+        image, label = self.resize_short_length(image, label, short_length=self.base_size)
+        image, label = self.gen_sample(image, label, self.multi_scale, self.flip)
+
+        return image.copy(), label.copy(), np.array(size), name
+
+    def save_pred(self, pred, sv_path, name):
+        print(pred) 
+        palette = self.get_palette(256)
+        preds = np.asarray(np.argmax(preds.cpu(), axis=1), dtype=np.uint8)
+        for i in range(preds.shape[0]):
+            pred = self.convert_label(preds[i], inverse=True)
+            save_img = Image.fromarray(pred)
+            save_img.putpalette(palette)
+            save_img.save(os.path.join(sv_path, name[i]+'.png'))
